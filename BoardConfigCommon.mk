9b4490839ab80d5b5156bacd6131ca976cba85ce
Kevin Davis <dumbandroid@gmail.com>
2012-08-01 07:50:50 -0700

    first

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,108 @@
+# Copyright (C) 2011 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# inherit from common msm7x30 Recovery
+-include device/htc/7x30-recovery/BoardConfigCommon.mk
+
+TARGET_NO_BOOTLOADER := true
+
+TARGET_BOARD_PLATFORM := msm7x30
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
+
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH_VARIANT := armv7-a-neon
+ARCH_ARM_HAVE_TLS_REGISTER := true
+ARCH_ARM_HAVE_VFP := true
+USE_MALLOC_ALIGNMENT := 16
+TARGET_HAVE_TSLIB := false
+
+TARGET_EXTRA_CFLAGS += $(call cc-option,-march=armv7-a -mtune=cortex-a8)
+TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
+TARGET_USE_SCORPION_BIONIC_OPTIMIZATION := true
+TARGET_USE_SCORPION_PLD_SET := true
+TARGET_SCORPION_BIONIC_PLDOFFS := 6
+TARGET_SCORPION_BIONIC_PLDSIZE := 128
+
+TARGET_SPECIFIC_HEADER_PATH := device/htc/msm7x30-common/include
+
+# Wifi related defines
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+WPA_SUPPLICANT_VERSION := VER_0_8_X
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+BOARD_WLAN_DEVICE := bcmdhd
+WIFI_DRIVER_FW_PATH_STA := "/vendor/firmware/fw_bcmdhd.bin"
+WIFI_DRIVER_FW_PATH_AP := "/vendor/firmware/fw_bcmdhd_apsta.bin"
+WIFI_DRIVER_FW_PATH_PARAM := "/sys/module/bcmdhd/parameters/firmware_path"
+WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/bcmdhd.ko"
+WIFI_DRIVER_MODULE_NAME := "bcmdhd"
+WIFI_DRIVER_MODULE_ARG := "firmware_path=/vendor/firmware/fw_bcmdhd.bin nvram_path=/proc/calibration iface_name=wlan"
+BOARD_WLAN_DEVICE_REV := bcmdhd
+WIFI_BAND := 802_11_ABG
+
+BOARD_USES_ADRENO_200 := true
+TARGET_FORCE_CPU_UPLOAD := true
+
+COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE -DSCREEN_RELEASE -DREFRESH_RATE=60 -DICS_CAMERA_BLOB -DQCOM_ICS_COMPAT -DRIL_VERSION_2_SUPPORT -DQCOM_NO_SECURE_PLAYBACK
+
+USE_OPENGL_RENDERER := true
+ENABLE_WEBGL := true
+TARGET_USES_C2D_COMPOSITION := false
+TARGET_USES_SF_BYPASS := true
+TARGET_HAVE_BYPASS := true
+TARGET_USES_OVERLAY := true
+TARGET_QCOM_HDMI_OUT := false
+TARGET_GRALLOC_USES_ASHMEM := true
+TARGET_USES_GENLOCK := true
+BOARD_ADRENO_DECIDE_TEXTURE_TARGET := true
+
+BOARD_NEEDS_MEMORYHEAPPMEM := true
+
+BOARD_PREBUILT_LIBAUDIO := false
+DYNAMIC_SHARED_LIBV8SO := true
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+
+BOARD_VENDOR_QCOM_AMSS_VERSION := 1200
+
+BOARD_EGL_CFG := device/htc/msm7x30-common/egl.cfg
+
+BOARD_USES_QCOM_HARDWARE := true
+BOARD_USES_QCOM_LIBS := true
+BOARD_USES_QCOM_LIBRPC := true
+BOARD_USES_QCOM_GPS := true
+BOARD_USE_QCOM_PMEM := true
+TARGET_USES_MDP3 := true
+TARGET_USES_PMEM := true
+BOARD_USES_HTC_CAMERA := true
+#TARGET_LEGACY_CAMERA := true
+
+#BOARD_USES_QCOM_AUDIO_SPEECH := true
+#BOARD_USES_QCOM_AUDIO_VOIPMUTE := true
+#BOARD_USES_QCOM_AUDIO_RESETALL := true
+#COMMON_GLOBAL_CFLAGS += -DWITH_QCOM_LPA
+#TARGET_USES_QCOM_LPA := true
+COMMON_GLOBAL_CFLAGS += -DQCOM_ROTATOR_KERNEL_FORMATS
+
+TARGET_BOOTANIMATION_PRELOAD := true
+TARGET_BOOTANIMATION_TEXTURE_CACHE := true
+
+BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 1240
+
+TARGET_BOOTANIMATION_PRELOAD := true
+TARGET_BOOTANIMATION_TEXTURE_CACHE := true
+
+BOARD_HAVE_FM_RADIO := true
+BOARD_GLOBAL_CFLAGS += -DHAVE_FM_RADIO
